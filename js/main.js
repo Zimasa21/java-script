08cfd88203bb3f7da13c4e3d937d08e7e46e0ccb
unknown <zngqetsu27@gmail.com>
2020-03-09 13:25:10 +0200

    adding

---
+++ js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let message ="please drink water";
+console.log(message);
+
+let name =prompt("enter firstname");
+console.log(name);
+
+let age =prompt("Enter Age");
+console.log(age);
+
+//conditional statment run//
+//code run if condition is true//
+//else run when condition is not true//
+
+let number =Number(prompt("Enter a number"));
+if (number > 0) {
+ console.log(number + "is positive");   
+} else if (number < 0) {
+ console.log(number + "is negative");
+} else {
+    console.log(number + "is zero");
+}
+
+//writting a program that people will decide what to wear due to weather condition//
+//using the if, else if, else//
+
+let weather =(prompt("Enter a weather"));
+if (weather ==="Sunny") {
+    console.log( weather="wear short");   
+} else if (weather==="rainy") {
+    console.log("wear raincoat");
+} else if (weather==="snowy") {
+    console.log(weather="do not drive, stay in doors")
+} else (weather=== "windy") 
+    console.log("stay away from open space")
+
+
